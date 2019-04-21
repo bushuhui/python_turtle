c94ad4099d934743b7572cb52ebff5c6e2a4d15a
Shuhui Bu <bushuhui@gmail.com>
2019-04-21 09:35:57 +0800

    Add spirograph demos

--- color_hexspial.py
+++ color_hexspial.py
@@ -5,11 +5,11 @@ colors = ['red', 'purple', 'blue',
 
 t = turtle.Pen()
 t.speed(0)
-turtle.bgcolor('black')
+turtle.bgcolor('white')
 for x in range(360):
     t.pencolor(colors[x%6])
     t.width(x/100+1)
     t.forward(x)
-    t.left(59)
+    t.left(61)
 
 turtle.done()
